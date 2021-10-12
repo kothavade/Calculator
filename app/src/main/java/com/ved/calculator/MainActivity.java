c633f6a85d61acd2491ccbbc2f521a95e74babe3
school computer <10021902@sbstudents.org>
2021-10-12 12:13:07 -0400

    define buttons and display in main

--- app/src/main/java/com/ved/calculator/MainActivity.java
+++ app/src/main/java/com/ved/calculator/MainActivity.java
@@ -5,16 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    TextView display;
     Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
+    Button nine;
+    Button zero;
+    Button plus;
+    Button minus;
+    Button multiply;
+    Button divide;
+    Button clear;
+    Button equal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        one=findViewById(R.id.one)
+
+        display=findViewById(R.id.display);
+        one=findViewById(R.id.one);
+        two=findViewById(R.id.two);
+        three=findViewById(R.id.three);
+        four=findViewById(R.id.four);
+        five=findViewById(R.id.five);
+        six=findViewById(R.id.six);
+        seven=findViewById(R.id.seven);
+        eight=findViewById(R.id.eight);
+        nine=findViewById(R.id.nine);
+        zero=findViewById(R.id.zero);
+        plus=findViewById(R.id.plus);
+        minus=findViewById(R.id.minus);
+        multiply=findViewById(R.id.multiply);
+        divide=findViewById(R.id.divide);
+        clear=findViewById(R.id.clear);
+        equal=findViewById(R.id.equal);
+
         one.setOnClickListener(this);
     }
 
