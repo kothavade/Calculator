f88990c267eebb18a845ad9b4dc5159300ca3807
school computer <10021902@sbstudents.org>
2021-10-12 12:00:34 -0400

    implement onclick listener

--- app/src/main/java/com/ved/calculator/MainActivity.java
+++ app/src/main/java/com/ved/calculator/MainActivity.java
@@ -3,12 +3,23 @@ package com.ved.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        one=findViewById(R.id.one)
+        one.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
