7ef166958c1d07467f0fd90c39437be83cafb4c4
school computer <10021902@sbstudents.org>
2021-10-14 12:17:27 -0400

    add buttons to switch

--- app/src/main/java/com/ved/calculator/MainActivity.java
+++ app/src/main/java/com/ved/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String numbers;
     TextView display;
     Button one;
     Button two;
@@ -67,14 +67,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clear.setOnClickListener(this);
         equal.setOnClickListener(this);
 
+        numbers="";
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.one:
+                numbers+="1";
                 break;
-
+            case R.id.two:
+                numbers+="2";
+                break;
+            case R.id.three:
+                numbers+="3";
+                break;
+            case R.id.four:
+                numbers+="4";
+                break;
+            case R.id.five:
+                numbers+="5";
+                break;
+            case R.id.six:
+                numbers+="6";
+                break;
+            case R.id.seven:
+                numbers+="7";
+                break;
+            case R.id.eight:
+                numbers+="8";
+                break;
+            case R.id.nine:
+                numbers+="9";
+                break;
+            case R.id.zero:
+                numbers+="0";
+                break;
+            case R.id.clear:
+                numbers+="c";
+                break;
+            case R.id.multiply:
+                numbers+="*";
+                break;
+            case R.id.divide:
+                numbers+="/";
+                break;
+            case R.id.plus:
+                numbers+="+";
+                break;
+            case R.id.minus:
+                numbers+="-";
+                break;
+            case R.id.equal:
+                numbers+="=";
+                break;
         }
     }
 }
